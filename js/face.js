c112dce776bd5dafefe6a67c1bd37b8a60ba5312
GUARIM <84085689+guarim@users.noreply.github.com>
2024-04-17 21:06:08 +0200

    Update face.js

--- js/face.js
+++ js/face.js
@@ -19,7 +19,7 @@ function onResultsFace(results) {
   if (results.detections.length > 0) {
     drawRectangle(
         canvasCtx1, results.detections[0].boundingBox,
-        {color: 'blue', lineWidth: 2, fillColor: '#00FFFFFF'});
+        {color: 'blue', lineWidth: 2, fillColor: '#00FFFF'});
     drawLandmarks(canvasCtx1, results.detections[0].landmarks, {
       color: 'white',
       radius: 0.5,
